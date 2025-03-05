716465b111786eb0219d26f722bfa6da35e954ea
Adil omer <36453919+adelazhary@users.noreply.github.com>
2025-03-05 17:45:14 +0800

    Update authentication logic and login request validation

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -2,14 +2,19 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Events\NewNotification;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\UpdateProfileRequest;
+use Carbon\Carbon;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Inertia\Response;
+use Intervention\Image\Laravel\Facades\Image;
+use Log;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -48,5 +53,45 @@ class AuthenticatedSessionController extends Controller
         $request->session()->regenerateToken();
 
         return redirect('/');
+    }
+
+    public function updateInfo(UpdateProfileRequest $request): RedirectResponse
+    {
+        $request->validated();
+
+        $user = auth()->user();
+
+        $user->update([
+            'name' => $request->name,
+            'email' => $request->email,
+            'middle_name' => $request->personal_details['middle_name'] ?? null,
+            'last_name' => $request->personal_details['last_name'] ?? null,
+            'preferred_name' => $request->personal_details['preferred_name'] ?? null,
+            'date_of_birth' => isset($request->personal_details['date_of_birth'])
+                ? Carbon::parse($request->personal_details['date_of_birth'])->format('Y-m-d')
+                : today(),
+            'gender' => $request->personal_details['gender'] ?? null,
+            'nationality' => $request->personal_details['nationality'] ?? null,
+        ]);
+
+        // Handle avatars upload
+        if ($request->hasFile('avatar')) {
+
+            $avatar = Image::read($request->avatar);
+            $imageName = time() . '-' . $request->file('avatar')->getClientOriginalName();
+            $destinationPath = public_path('images/avatars');
+            $avatar->save($destinationPath . $imageName);
+
+            $destinationPathThumbnail = public_path('images/avatars');
+            $avatar->resize(100, 100);
+            $avatar->save($destinationPathThumbnail . $imageName);
+
+            $user->update(['avatar' => $imageName]);
+        }
+
+        event(new NewNotification('Profile updated successfully'));
+//       Debug: Log the event
+        Log::info('NewNotification event triggered');
+        return redirect()->back()->with('success', 'Profile updated successfully!');
     }
 }
